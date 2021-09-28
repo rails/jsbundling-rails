a077400ea1211d213ae2f09428fdcb03081ce452
thomasvanholder <thomasvanholder@hey.com>
2021-09-28 19:13:11 +0100

    meaninful error message when NPX is < 7.1 - webpack

--- lib/install/webpack/install.rb
+++ lib/install/webpack/install.rb
@@ -1,3 +1,11 @@
+stdout, _stderr, _status = Open3.capture3("npx -v")
+npx_version = stdout.match(/\d.\d+/).to_s.to_f
+
+if npx_version < 7.1
+  say "Failed. Upgrade npx to version 7.1 or later and run install script again", :red
+  abort
+end
+
 say "Install Webpack with config"
 copy_file "#{__dir__}/webpack.config.js", "webpack.config.js"
 run "yarn add webpack webpack-cli"
