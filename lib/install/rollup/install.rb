0cc65be6552ac366d9082251c9cca23e74e0adce
thomasvanholder <thomasvanholder@hey.com>
2021-09-28 19:12:50 +0100

    meaninful error message when NPX is < 7.1 - rollup

--- lib/install/rollup/install.rb
+++ lib/install/rollup/install.rb
@@ -1,3 +1,11 @@
+stdout, _stderr, _status = Open3.capture3("npx -v")
+npx_version = stdout.match(/\d.\d+/).to_s.to_f
+
+if npx_version < 7.1
+  say "Failed. Upgrade npx to version 7.1 or later and run install script again", :red
+  abort
+end
+
 say "Install rollup with config"
 copy_file "#{__dir__}/rollup.config.js", "rollup.config.js"
 run "yarn add rollup @rollup/plugin-node-resolve"
