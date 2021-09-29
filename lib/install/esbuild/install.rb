c3c4610c35b339dc703afbf29baa2485f5668941
thomasvanholder <36309895+thomasvanholder@users.noreply.github.com>
2021-09-30 00:35:46 +0100

    adding missing curly bracket (#39)

--- lib/install/esbuild/install.rb
+++ lib/install/esbuild/install.rb
@@ -5,7 +5,7 @@ say "Add build script"
 build_script = "esbuild app/javascript/*.* --bundle --outdir=app/assets/builds"
 
 if (`npx -v`.to_f < 7.1 rescue "Missing")
-  say %(Add "scripts": { "build": "#{build_script}" to your package.json), :green
+  say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
 else
   run %(npm set-script build "#{build_script}")
 end
--- lib/install/rollup/install.rb
+++ lib/install/rollup/install.rb
@@ -6,7 +6,7 @@ say "Add build script"
 build_script = "rollup -c rollup.config.js"
 
 if (`npx -v`.to_f < 7.1 rescue "Missing")
-  say %(Add "scripts": { "build": "#{build_script}" to your package.json), :green
+  say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
 else
   run %(npm set-script build "#{build_script}")
 end
