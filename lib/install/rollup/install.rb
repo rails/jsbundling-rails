36797171aac706458586a6c0ae79ba207f836104
David Heinemeier Hansson <david@hey.com>
2021-12-03 11:42:18 +0100

    Always run build on first install
    So builds/application.js is not missing.

--- lib/install/rollup/install.rb
+++ lib/install/rollup/install.rb
@@ -9,4 +9,5 @@ if (`npx -v`.to_f < 7.1 rescue "Missing")
   say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
 else
   run %(npm set-script build "#{build_script}")
+  run %(yarn build)
 end
