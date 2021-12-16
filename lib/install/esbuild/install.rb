2cc5e8943633e611479df8a9d81ff5402bc7e21d
Richard K. Miller <richard@richardkmiller.com>
2021-12-15 18:57:36 -0700

    Adding --public-path option to the esbuild script
    Setting `esbuild ... --public-path=assets` allows client-side code to reference images from the asset pipeline.
    
    Example:
    1. Create image at `app/javascript/images/example.png`.
    2. Add esbuild option `--loader:.png=file` (not included in this PR).
    3. When esbuild runs, it will output e.g. `app/assets/builds/example-5SRKKTLZ.png`.
    4. In frontend code, e.g. a React component, add `import Example from "../images/example.png"` .
    5. Now the image can be referenced with `<img src={Example} />`.
    6. The path resolves to /assets/example-5SRKKTLZ.png. Without this PR, the path resolves to /example-5SRKKTLZ.png.

--- lib/install/esbuild/install.rb
+++ lib/install/esbuild/install.rb
@@ -2,7 +2,7 @@ say "Install esbuild"
 run "yarn add esbuild"
 
 say "Add build script"
-build_script = "esbuild app/javascript/*.* --bundle --sourcemap --outdir=app/assets/builds"
+build_script = "esbuild app/javascript/*.* --bundle --sourcemap --outdir=app/assets/builds --public-path=assets"
 
 if (`npx -v`.to_f < 7.1 rescue "Missing")
   say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
