3e745dd662376d95043fbf6f6d913e372f299e7c
Abe Voelker <_@abevoelker.com>
2022-08-01 21:39:16 -0500

    Handle subdirectories when building with esbuild

--- lib/install/esbuild/install.rb
+++ lib/install/esbuild/install.rb
@@ -1,8 +1,9 @@
 say "Install esbuild"
+copy_file "#{__dir__}/tsconfig.json", "tsconfig.json"
 run "yarn add esbuild"
 
 say "Add build script"
-build_script = "esbuild app/javascript/*.* --bundle --sourcemap --outdir=app/assets/builds --public-path=assets"
+build_script = "esbuild \`find app/javascript -type f\` --tsconfig=./tsconfig.json --bundle --sourcemap --outdir=app/assets/builds --public-path=assets"
 
 if (`npx -v`.to_f < 7.1 rescue "Missing")
   say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
