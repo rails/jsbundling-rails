4ec3041a4aa7fdf28287f9dec5e3b6966b8b15d4
Justin Coyne <jcoyne@justincoyne.com>
2022-11-29 08:31:31 -0600

    Add support for npm 9+
    Fixes rails/jsbundling-rails#138

--- lib/install/esbuild/install.rb
+++ lib/install/esbuild/install.rb
@@ -4,9 +4,16 @@ run "yarn add esbuild"
 say "Add build script"
 build_script = "esbuild app/javascript/*.* --bundle --sourcemap --outdir=app/assets/builds --public-path=assets"
 
-if (`npx -v`.to_f < 7.1 rescue "Missing")
-  say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
-else
+npm_supports_set_script = ->(n) { n >= 7.1 && n < 8.0 }
+npm_supports_pkg_set = ->(n) { n >= 8.0 }
+
+case `npx -v`.to_f
+when npm_supports_pkg_set
+  run %(npm pkg set scripts.build="#{build_script}")
+  run %(yarn build)
+when npm_supports_set_script
   run %(npm set-script build "#{build_script}")
   run %(yarn build)
+else
+  say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
 end
--- lib/install/rollup/install.rb
+++ lib/install/rollup/install.rb
@@ -5,9 +5,16 @@ run "yarn add rollup @rollup/plugin-node-resolve"
 say "Add build script"
 build_script = "rollup -c rollup.config.js"
 
-if (`npx -v`.to_f < 7.1 rescue "Missing")
-  say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
-else
+npm_supports_set_script = ->(n) { n >= 7.1 && n < 8.0 }
+npm_supports_pkg_set = ->(n) { n >= 8.0 }
+
+case `npx -v`.to_f
+when npm_supports_pkg_set
+  run %(npm pkg set scripts.build="#{build_script}")
+  run %(yarn build)
+when npm_supports_set_script
   run %(npm set-script build "#{build_script}")
   run %(yarn build)
+else
+  say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
 end
