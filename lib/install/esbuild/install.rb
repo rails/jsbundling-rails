b924206d178f3f8c72ef3b2f891f095601df6328
David Heinemeier Hansson <david@basecamp.com>
2022-12-14 14:09:25 +0100

    Simplify case

--- lib/install/esbuild/install.rb
+++ lib/install/esbuild/install.rb
@@ -4,15 +4,12 @@ run "yarn add esbuild"
 say "Add build script"
 build_script = "esbuild app/javascript/*.* --bundle --sourcemap --outdir=app/assets/builds --public-path=assets"
 
-npm_supports_set_script = ->(n) { n >= 7.1 && n < 8.0 }
-npm_supports_pkg_set = ->(n) { n >= 8.0 }
-
 case `npx -v`.to_f
-when npm_supports_pkg_set
-  run %(npm pkg set scripts.build="#{build_script}")
-  run %(yarn build)
-when npm_supports_set_script
+when 7.1...8.0
   run %(npm set-script build "#{build_script}")
+  run %(yarn build)
+when 8.0..
+  run %(npm pkg set scripts.build="#{build_script}")
   run %(yarn build)
 else
   say %(Add "scripts": { "build": "#{build_script}" } to your package.json), :green
