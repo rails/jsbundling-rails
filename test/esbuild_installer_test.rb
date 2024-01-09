a9ff3f74bf5f387f79be036858d57af54202520f
Jonathan Hefner <jonathan@hefner.pro>
2024-01-09 17:05:17 -0600

    Test specific behavior of javascript:install:esbuild
    This adds test coverage for specific behavior of the
    `javascript:install:esbuild` command.

--- test/esbuild_installer_test.rb
+++ test/esbuild_installer_test.rb
@@ -5,6 +5,21 @@ class EsbuildInstallerTest < ActiveSupport::TestCase
   include RailsAppHelpers
   include SharedInstallerTests
 
+  test "esbuild installer" do
+    with_new_rails_app do
+      out, _err = run_installer
+
+      File.read("Procfile.dev").tap do |procfile|
+        assert_match "js: yarn build --watch", procfile
+      end
+
+      assert_match "STUBBED gem install foreman", out
+      assert_match %r{STUBBED yarn add.* esbuild}, out
+      assert_match %r{STUBBED npm (?:set-script build |pkg set scripts.build=)esbuild app/javascript/\*\.\*}, out
+      assert_match "STUBBED yarn build", out
+    end
+  end
+
   private
     def run_installer
       stub_bins("gem", "yarn", "npm")
