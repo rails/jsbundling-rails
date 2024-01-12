24e88caa9a929c207fbbcf662906d2c692a48b54
zzak <zzakscott@gmail.com>
2024-01-12 17:27:01 +0900

    Move webpack tests to a subdirectory

---
+++ test/installer/webpack_test.rb
@@ -0,0 +1,35 @@
+require "test_helper"
+
+class WebpackInstallerTest < ActiveSupport::TestCase
+  include RailsAppHelpers
+
+  test "installer for webpack" do
+    with_new_rails_app do
+      _out, _err = run_command("bin/rails", "javascript:install:webpack")
+
+      assert_equal File.read("#{__dir__}/../../lib/install/Procfile.dev"), File.read("Procfile.dev")
+      assert_equal File.read("#{__dir__}/../../lib/install/webpack/webpack.config.js"), File.read("webpack.config.js")
+
+      JSON.parse(File.read("package.json")).tap do |package|
+        assert_not_nil package["dependencies"]["webpack"]
+        assert_not_nil package["dependencies"]["webpack-cli"]
+        assert_equal "webpack --config webpack.config.js", package["scripts"]["build"]
+      end
+
+      if sprockets?
+        assert_match "//= link_tree ../images", File.read("app/assets/config/manifest.js")
+        assert_match "//= link_directory ../stylesheets .css", File.read("app/assets/config/manifest.js")
+        assert_match "//= link_tree ../builds", File.read("app/assets/config/manifest.js")
+      end
+    end
+  end
+
+  test "installer with pre-existing Procfile" do
+    with_new_rails_app do
+      File.write("Procfile.dev", "pre: existing\n")
+      _out, _err = run_command("bin/rails", "javascript:install:webpack")
+
+      assert_equal "pre: existing\njs: yarn build --watch\n", File.read("Procfile.dev")
+    end
+  end
+end
--- test/installer_test.rb
+++ test/installer_test.rb
@@ -5,7 +5,7 @@ class InstallerTest < ActiveSupport::TestCase
 
   test "installer" do
     with_new_rails_app do
-      out, _err = run_command("bin/rails", "javascript:install:shared")
+      _out, _err = run_command("bin/rails", "javascript:install:shared")
 
       assert_equal 0, File.size("app/assets/builds/.keep")
       assert_match "/app/assets/builds/*\n!/app/assets/builds/.keep", File.read(".gitignore")
@@ -22,24 +22,9 @@ class InstallerTest < ActiveSupport::TestCase
     end
   end
 
-  test "installer for webpack" do
-    with_new_rails_app do
-      out, _err = run_command("bin/rails", "javascript:install:webpack")
-
-      assert_equal File.read("#{__dir__}/../lib/install/Procfile.dev"), File.read("Procfile.dev")
-      assert_equal File.read("#{__dir__}/../lib/install/webpack/webpack.config.js"), File.read("webpack.config.js")
-
-      if sprockets?
-        assert_match "//= link_tree ../images", File.read("app/assets/config/manifest.js")
-        assert_match "//= link_directory ../stylesheets .css", File.read("app/assets/config/manifest.js")
-        assert_match "//= link_tree ../builds", File.read("app/assets/config/manifest.js")
-      end
-    end
-  end
-
   test "installer with .gitignore" do
     with_new_rails_app do
-      out, _err = run_command("bin/rails", "javascript:install:webpack")
+      _out, _err = run_command("bin/rails", "javascript:install:shared")
 
       assert_match %(/app/assets/builds/*\n!/app/assets/builds/.keep), File.read(".gitignore")
     end
@@ -48,7 +33,7 @@ class InstallerTest < ActiveSupport::TestCase
   test "installer with missing .gitignore" do
     with_new_rails_app do
       FileUtils.rm(".gitignore")
-      out, _err = run_command("bin/rails", "javascript:install:webpack")
+      _out, _err = run_command("bin/rails", "javascript:install:shared")
 
       assert_not File.exist?(".gitignore")
     end
@@ -56,7 +41,7 @@ class InstallerTest < ActiveSupport::TestCase
 
   test "installer with pre-existing layouts/application.html.erb" do
     with_new_rails_app do
-      out, _err = run_command("bin/rails", "javascript:install:webpack")
+      out, _err = run_command("bin/rails", "javascript:install:shared")
 
       assert_match %(Add JavaScript include tag in application layout), out
       assert_match %(<%= javascript_include_tag "application", "data-turbo-track": "reload", type: "module" %>), File.read("app/views/layouts/application.html.erb")
@@ -66,19 +51,10 @@ class InstallerTest < ActiveSupport::TestCase
   test "installer with missing layouts/application.html.erb" do
     with_new_rails_app do
       FileUtils.rm("app/views/layouts/application.html.erb")
-      out, _err = run_command("bin/rails", "javascript:install:webpack")
+      out, _err = run_command("bin/rails", "javascript:install:shared")
 
       assert_match %(Default application.html.erb is missing!), out
       assert_match %(Add <%= javascript_include_tag "application", "data-turbo-track": "reload", type: "module" %> within the <head> tag in your custom layout.), out
-    end
-  end
-
-  test "installer with pre-existing Procfile" do
-    with_new_rails_app do
-      File.write("Procfile.dev", "pre: existing\n")
-      out, _err = run_command("bin/rails", "javascript:install:webpack")
-
-      assert_equal "pre: existing\njs: yarn build --watch\n", File.read("Procfile.dev")
     end
   end
 
