3b3ffbc2468e41cde31ed134cd2dc06280218a80
David Heinemeier Hansson <david@hey.com>
2024-07-29 11:20:51 -0700

    Force bin/dev overwrite to deal with new default bin/dev file
    See Rails #52433

--- lib/install/install.rb
+++ lib/install/install.rb
@@ -32,5 +32,5 @@ unless Rails.root.join("package.json").exist?
 end
 
 say "Add bin/dev to start foreman"
-copy_file "#{__dir__}/dev", "bin/dev"
+copy_file "#{__dir__}/dev", "bin/dev", force: true
 chmod "bin/dev", 0755, verbose: false
