2413e21cd6d6c42bbaad1d3275b50faac63707ba
Jonathan Hefner <jonathan@hefner.pro>
2024-07-31 15:50:40 -0500

    Test installer commands (retry) (#197)
    * Add testing infrastructure
    
    This adds testing infrastructure to test against multiple versions of
    Rails, including Rails `main` branch.
    
    * Test shared behavior of installer commands
    
    This adds test coverage for the shared behavior of the installer
    commands.  The commands are tested against a freshly generated Rails app
    using the version of Rails that is currently loaded.  Thus the
    installers can be tested with different versions of Rails in CI.
    
    * Test specific behavior of javascript:install:bun
    
    This adds test coverage for specific behavior of the
    `javascript:install:bun` command.
    
    * Test specific behavior of javascript:install:esbuild
    
    This adds test coverage for specific behavior of the
    `javascript:install:esbuild` command.
    
    * Test specific behavior of javascript:install:rollup
    
    This adds test coverage for specific behavior of the
    `javascript:install:rollup` command.
    
    * Test specific behavior of javascript:install:webpack
    
    This adds test coverage for specific behavior of the
    `javascript:install:webpack` command.

---
+++ test/rollup_installer_test.rb
@@ -0,0 +1,31 @@
+require "test_helper"
+require_relative "shared_installer_tests"
+
+class RollupInstallerTest < ActiveSupport::TestCase
+  include RailsAppHelpers
+  include SharedInstallerTests
+
+  test "rollup installer" do
+    with_new_rails_app do
+      out, _err = run_installer
+
+      File.read("Procfile.dev").tap do |procfile|
+        assert_match "js: yarn build --watch", procfile
+      end
+
+      assert_match "STUBBED gem install foreman", out
+
+      assert File.exist?("rollup.config.js")
+
+      assert_match %r{STUBBED yarn add.* rollup}, out
+      assert_match %r{STUBBED npm (?:set-script build |pkg set scripts.build=)rollup .*rollup.config.js}, out
+      assert_match "STUBBED yarn build", out
+    end
+  end
+
+  private
+    def run_installer
+      stub_bins("gem", "yarn", "npm")
+      run_command("bin/rails", "javascript:install:rollup")
+    end
+end
