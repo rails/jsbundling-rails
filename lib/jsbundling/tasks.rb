30477fdbd195f0054896377864c68eb3e4801d47
Zach Ahn <engineering@zachahn.com>
2024-09-22 00:41:20 -0400

    Move `Jsbundling::Tasks` into its own file

---
+++ lib/jsbundling/tasks.rb
@@ -0,0 +1,42 @@
+module Jsbundling
+  module Tasks
+    extend self
+
+    def install_command
+      case tool
+      when :bun then "bun install"
+      when :yarn then "yarn install"
+      when :pnpm then "pnpm install"
+      when :npm then "npm install"
+      else raise "jsbundling-rails: No suitable tool found for installing JavaScript dependencies"
+      end
+    end
+
+    def build_command
+      case tool
+      when :bun then "bun run build"
+      when :yarn then "yarn build"
+      when :pnpm then "pnpm build"
+      when :npm then "npm run build"
+      else raise "jsbundling-rails: No suitable tool found for building JavaScript"
+      end
+    end
+
+    def tool_exists?(tool)
+      system "command -v #{tool} > /dev/null"
+    end
+
+    def tool
+      case
+      when File.exist?('bun.lockb') then :bun
+      when File.exist?('yarn.lock') then :yarn
+      when File.exist?('pnpm-lock.yaml') then :pnpm
+      when File.exist?('package-lock.json') then :npm
+      when tool_exists?('bun') then :bun
+      when tool_exists?('yarn') then :yarn
+      when tool_exists?('pnpm') then :pnpm
+      when tool_exists?('npm') then :npm
+      end
+    end
+  end
+end
